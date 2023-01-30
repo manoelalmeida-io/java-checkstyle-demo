6896a45b5e7d1aa693f79529b0b32a4fcb52d713
Manoel Almeida <manoel.almeida@sptech.school>
2023-01-30 12:09:15 -0300

    Project initialization

---
+++ src/main/java/school/sptech/checkstyletest/CheckstyleTestApplication.java
@@ -0,0 +1,15 @@
+package school.sptech.checkstyletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot main file.
+ */
+@SpringBootApplication
+public class CheckstyleTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CheckstyleTestApplication.class, args);
+  }
+}
---
+++ src/test/java/school/sptech/checkstyletest/CheckstyleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.checkstyletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
